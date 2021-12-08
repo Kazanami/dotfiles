76ab440a2dc758a5ffbca50b46a9cbb14342b36b
Kazanami <dummy.github.1945@gmail.com>
2021-12-08 16:09:29 +0900

    [main] installer logo added

--- bin/link.sh
+++ bin/link.sh
@@ -3,16 +3,24 @@
 # dotfiles root dir
 dotfiles_root=$(cd $(dirname $0)/.. && pwd)
 LINK_LISTS="${dotfiles_root}/list"
+dot_ascii='
+    _     _    __ _ _        
+ __| |___| |_ / _(_) |___ ___
+/ _` / _ \  _|  _| | / -_|_-<
+\__,_\___/\__|_| |_|_\___/__/
+                             '
 
 # Linrary load
 LIB_BASE="${dotfiles_root}/lib"
 source ${LIB_BASE}/logger.sh
 source ${LIB_BASE}/common.sh
 
+echo "${dot_ascii}"
+echo "Welcome to Kazanami's dotfiles installer"
+
 # Main
 for linklist in "${LINK_LISTS}/linklist.Unix.txt" "${LINK_LISTS}/linklist.$(uname).txt"; do
     [ ! -r "${linklist}" ] && continue
-    _remove_linklist_comment "$linklist"
     _remove_linklist_comment "$linklist" | while read target link; do
  #       echo "${target} ${link}"
         target=$(eval echo "${dotfiles_root}/${target}")
